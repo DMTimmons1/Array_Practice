fb726dd63ceb18412452159e3d834b6349ba0f98
Dawson Timmons <DawsonTimmons@gmail.com>
2022-11-02 14:20:50 -0600

    Add is_working array

--- arrays.rb
+++ arrays.rb
@@ -4,4 +4,6 @@ friend_names ["Joseph", "Aidan", "Matt", "Mike"]
 
 friend_ages [22, 24, 27, 21]
 
-friend_weight [175.33, 144.26, 210.10, 189.40]
+friend_weights [175.33, 144.26, 210.10, 189.40]
+
+is_working [true, true, false, true]
